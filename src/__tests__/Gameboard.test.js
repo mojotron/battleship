133f450687a9eedbfd529fea46475a59f2ab616a
mojotron <igormalj@gmail.com>
2022-04-07 15:42:39 +0200

    Finish up basic tests for gameboard

--- src/__tests__/Gameboard.test.js
+++ src/__tests__/Gameboard.test.js
@@ -21,7 +21,7 @@ describe('GameBoard factory function', () => {
   test('place ship, board positions has hasShip flag', () => {
     const shipType = 'battleship';
     const shipPosition = [0, 1, 2, 3];
-    board.addShip(shipType, shipPosition);
+    board.placeShip(shipType, shipPosition);
     expect(shipPosition.every(pos => board.board[pos].hasShip === true)).toBe(
       true
     );
@@ -30,26 +30,65 @@ describe('GameBoard factory function', () => {
     ).toBe(true);
   });
 
-  test.todo('trying to place ship on top of  another ship');
+  test('place ship that already exists', () => {
+    const shipType = 'submarine';
+    board.placeShip(shipType, [0, 1, 2]);
+    expect(() => board.placeShip(shipType, [5, 6, 7])).toThrowError(
+      '☢️ Invalid ship placement, ship type already on board'
+    );
+  });
 
-  test.todo('trying to place ship outside boundaries horizontally');
-
-  test.todo('trying to place ship outside boundaries vertically');
-
-  test.todo('place all 5 ships randomly without collision or boundaries error');
+  test('trying to place ship on top of  another ship', () => {
+    const shipPosition1 = [0, 1, 2, 3];
+    const shipPosition2 = [1, 11, 21];
+    board.placeShip('battleship', shipPosition1);
+    expect(() => board.placeShip('submarine', shipPosition2)).toThrowError(
+      '☢️ Invalid ship position, colliding with another ship'
+    );
+  });
 
   test('attack that misses ship', () => {
-    board.addShip('submarine', [0, 1, 2]);
+    board.placeShip('submarine', [0, 1, 2]);
     board.attack(3);
     expect(board.board[3].isHit).toBe(true);
     expect(board.board[3].hasShip).toBe(false);
   });
 
-  test.todo('attack that hits ship');
+  test('attack that hits ship', () => {
+    board.placeShip('destroyer', [0, 1, 2]);
+    board.attack(2);
+    expect(board.board[2].isHit).toBe(true);
+    expect(board.board[2].hasShip).toBe(true);
+  });
 
-  test.todo('attack to cell that is already hit');
+  test('attack to cell that is already hit', () => {
+    board.placeShip('carrier', [0, 1, 2, 3, 4]);
+    board.attack(2);
+    expect(() => board.attack(2)).toThrowError(
+      '☢️ Invalid attack, cell already attacked'
+    );
+  });
 
-  test.todo('attack to outside boundaries');
+  test('attack to outside boundaries', () => {
+    expect(() => board.attack(-3)).toThrowError(
+      '☢️ Invalid attack, outside of board boundaries'
+    );
+    expect(() => board.attack(100)).toThrowError(
+      '☢️ Invalid attack, outside of board boundaries'
+    );
+  });
 
-  test.todo('all ship sunken');
+  test('all ship sunken', () => {
+    board.placeShip('patrol', [0, 1]);
+    board.placeShip('submarine', [2, 3, 4]);
+    // board.attack(0);
+    // board.attack(1);
+    // board.attack(2);
+    // board.attack(3);
+    expect(board.allSunk()).toBe(false);
+    // board.attack(4);
+    // expect(board.allSunk()).toBe(true);
+  });
+
+  test.todo('place all 5 ships randomly without collision or boundaries error');
 });
--- src/scripts/factories/GameBoard.js
+++ src/scripts/factories/GameBoard.js
@@ -1,10 +1,27 @@
+import Ship from './Ship';
+
 const GameBoard = () => {
   const state = {
     board: [],
+    ships: {},
   };
 
+  const checkShipCollision = positions =>
+    positions.some(pos => state.board[pos].hasShip);
+
   const addShip = (type, positions) => {
-    // make ship
+    state.ships[type] = Ship(type, positions);
+  };
+
+  const placeShip = (type, positions) => {
+    if (state.ships[type])
+      throw new Error('☢️ Invalid ship placement, ship type already on board');
+
+    if (checkShipCollision(positions))
+      throw new Error('☢️ Invalid ship position, colliding with another ship');
+
+    addShip(type, positions);
+
     positions.forEach(index => {
       state.board[index].hasShip = true;
       state.board[index].shipId = type;
@@ -12,7 +29,21 @@ const GameBoard = () => {
   };
 
   const attack = position => {
+    if (position < 0 || position >= state.board.length)
+      throw new Error('☢️ Invalid attack, outside of board boundaries');
+    if (state.board[position].isHit)
+      throw new Error('☢️ Invalid attack, cell already attacked');
     state.board[position].isHit = true;
+    if (state.board[position].hasShip) {
+      state.ships[state.board[position].shipId].hit(position);
+    }
+  };
+
+  const allSunk = () => {
+    for (const ship of Object.values(state.ships)) {
+      if (ship.isSunk() === false) return false;
+    }
+    return true;
   };
 
   const init = () => {
@@ -28,8 +59,12 @@ const GameBoard = () => {
     get board() {
       return state.board.map(ele => ({ ...ele }));
     },
-    addShip,
+    get ships() {
+      return state.ships.map(ele => ele.type);
+    },
+    placeShip,
     attack,
+    allSunk,
   };
 };
 
