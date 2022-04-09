6787c85b65703fd5840f3e5d13bce4414f11d9f4
mojotron <igormalj@gmail.com>
2022-04-09 12:24:41 +0200

    Add basic test for ai player - add super basic ai implementation

---
+++ src/__tests__/AiPlayer.test.js
@@ -0,0 +1,41 @@
+import AiPlayer from '../scripts/factories/AiPlayer';
+
+describe('Computer player factory', () => {
+  let ai;
+
+  beforeEach(() => {
+    ai = AiPlayer();
+  });
+
+  test('create new ai player', () => {
+    const ships = ai.board.filter(cell => cell.hasShip).length;
+    expect(ai.board.length).toBe(100);
+    expect(ships).toBe(17);
+  });
+
+  test('keep track of available moves after attack', () => {
+    // test attack method side effect of removing available move
+    expect(ai.availableAttacks.length).toBe(100);
+    ai.attack();
+    expect(ai.availableAttacks.length).toBe(99);
+    ai.attack();
+    expect(ai.availableAttacks.length).toBe(98);
+  });
+
+  test('no more available moves', () => {
+    for (let i = 0; i < 100; i += 1) ai.attack();
+    expect(ai.attack()).toBe(-1);
+  });
+
+  test('attack returns integer in range', () => {
+    const attack1 = ai.attack();
+    expect(attack1).toBeGreaterThanOrEqual(0);
+    expect(attack1).toBeLessThan(100);
+    const attack2 = ai.attack();
+    expect(attack2).toBeGreaterThanOrEqual(0);
+    expect(attack2).toBeLessThan(100);
+    const attack3 = ai.attack();
+    expect(attack3).toBeGreaterThanOrEqual(0);
+    expect(attack3).toBeLessThan(100);
+  });
+});
---
+++ src/scripts/factories/AiPlayer.js
@@ -0,0 +1,28 @@
+import GameBoard from './GameBoard';
+
+const AiPlayer = () => {
+  const playerBoard = GameBoard();
+  playerBoard.createAndPlaceShips();
+  const availableAttacks = Array.from({ length: 100 }, (_, i) => i);
+
+  const attack = () => {
+    if (availableAttacks.length === 0) return -1;
+    const index = Math.floor(Math.random() * availableAttacks.length);
+    const attackPosition = availableAttacks.splice(index, 1);
+    return attackPosition.pop();
+  };
+
+  return {
+    get board() {
+      return playerBoard.board.map(cell => ({ ...cell }));
+    },
+
+    get availableAttacks() {
+      return [...availableAttacks];
+    },
+
+    attack,
+  };
+};
+
+export default AiPlayer;
